479d6e06fd1412dc97d9db1f0bd0c4382a82369f
Ardenile <81862570+vj9898@users.noreply.github.com>
2022-05-06 15:11:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+ 
+}
+
+function setup() 
+{ canvas = createCanvas(300, 300); 
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+
+function draw(){
+    
+}
